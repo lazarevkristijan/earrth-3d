1c8b55d5fa14906fa7d63a82c2019fae3f915e8d
lazarevkristijan <lazarevkristijan@gmail.com>
2024-08-11 19:37:45 +0300

    add sunlight and clouds

--- index.js
+++ index.js
@@ -20,25 +20,46 @@ controls.enableDamping = true
 controls.dampingFactor = 0.03
 
 const loader = new THREE.TextureLoader()
-const geometry = new THREE.IcosahedronGeometry(1, 12)
+const detail = 12
+const ball = new THREE.IcosahedronGeometry(1, detail)
 const material = new THREE.MeshStandardMaterial({
-  map: loader.load("./assets/earthnasa.jpg"),
+  map: loader.load("./assets/earthmap10k.jpg"),
   // map: loader.load("./assets/earthmap10k.jpg"),
 })
-const earthMesh = new THREE.Mesh(geometry, material)
+const earthMesh = new THREE.Mesh(ball, material)
 earthGroup.add(earthMesh)
 
-const stars = getStarField({ numStars: 3000 })
-console.log(stars)
+const cloudsMaterial = new THREE.MeshStandardMaterial({
+  map: loader.load("./assets/earthcloudmap2.jpg"),
+  blending: THREE.AdditiveBlending,
+  transparent: true,
+  opacity: 0.2,
+})
+const cloudsMesh = new THREE.Mesh(ball, cloudsMaterial)
+cloudsMesh.scale.setScalar(1.003)
+earthGroup.add(cloudsMesh)
+
+const lightsMaterial = new THREE.MeshBasicMaterial({
+  map: loader.load("./assets/earthnasa.jpg"),
+  blending: THREE.AdditiveBlending,
+})
+const lightsMesh = new THREE.Mesh(ball, lightsMaterial)
+earthGroup.add(lightsMesh)
+const stars = getStarField({ numStars: 5000 })
 scene.add(stars)
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffaaff)
-earthGroup.add(hemiLight)
+
+
+const sunLight = new THREE.DirectionalLight(0xffffff)
+sunLight.position.set(-2, 0.5, 2)
+scene.add(sunLight)
 
 const animate = () => {
   requestAnimationFrame(animate)
 
   earthMesh.rotation.y += 0.002
+  lightsMesh.rotation.y += 0.002
+  cloudsMesh.rotation.y += 0.002
   renderer.render(scene, camera)
   controls.update()
 }
